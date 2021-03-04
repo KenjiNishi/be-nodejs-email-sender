61d1949a25fdf07da320935b901fc47cf5e61484
KenjiNishi <wk.nishizawa@gmail.com>
2021-03-04 14:32:46 -0300

    get Surveys method

--- src/controllers/SurveyController.ts
+++ src/controllers/SurveyController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, response, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { SurveyRepository } from '../repositories/SurveyRepository';
 
@@ -24,6 +24,20 @@ class SurveyController{
         await surveyRepository.save(survey);
 
         return response.status(201).json(survey);
+    }
+
+    async show(req: Request, res: Response){
+        const surveyRepository = getCustomRepository(SurveyRepository);
+        const all = await surveyRepository.find();
+
+        if(all.length>0){
+            return res.status(200).json(all);
+        }
+        else{
+            return res.status(400).json({
+                error:"Empty!!!"
+            })
+        }
     }
 }
 
