80efd863e7f96c461a434951ec2da6cba3cddbca
KenjiNishi <wk.nishizawa@gmail.com>
2021-03-04 04:27:19 -0300

    routes, controllers and models basic structure

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import express from 'express';
 import 'reflect-metadata';
 
 import './database';
+import { router } from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(router);
 
 app.listen(8080, ()=>{
     console.log(`Server is running!`);
