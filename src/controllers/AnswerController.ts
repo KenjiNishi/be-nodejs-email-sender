e7b6691634ae38aed8cf2bc8c9709f69710af667
KenjiNishi <wk.nishizawa@gmail.com>
2021-03-05 11:17:32 -0300

    answers route and controller to manage the autogenerated survey answers on email and update SurveyUser table

---
+++ src/controllers/AnswerController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveyUserRepository } from "../repositories/SurveyUserRepository";
+
+class AnswerController{
+    // eg http://localhost:8080/answers/2?u=03b4b440-46cf-4148-9ff6-de8ebf30c9f0
+    async execute(request: Request, response: Response){
+        const {value} = request.params;
+        const {u} = request.query;
+
+        const surveyUserRepository = getCustomRepository(SurveyUserRepository);
+
+        const surveyUser = await surveyUserRepository.findOne({
+            id: String(u)
+        })
+        if (!surveyUser){
+            return response.status(400).json({error:"SurveyUser does not exist!"})
+        }
+
+        surveyUser.value = Number(value);
+        await surveyUserRepository.save(surveyUser);
+        return response.status(200).json(surveyUser);
+    }
+}
+export {AnswerController};
--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import {Router} from 'express';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveyController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
+import { AnswerController } from './controllers/AnswerController';
 
 const router = Router();
 const userController = new UserController();
 const surveyController = new SurveyController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.post("/users", userController.create);
 
@@ -14,5 +16,7 @@ router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
 
 router.post("/sendMail", sendMailController.execute)
+
+router.get("/answers/:value", answerController.execute);
 
 export {router};
